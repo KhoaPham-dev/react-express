cf3ca53acf82857beddd22ba7822acf1bc7f713b
KhoaPham <superstars1860@gmail.com>
2020-05-28 17:52:39 +0700

    sas

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express')
 
 const app = express()
 const path = require('path');
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.get('/api/a', (req, res) => {
     res.json({sayHi: 'hello from server, nice to meet you!'})
   })
 app.get('/*', (req, res) => {
- res.sendFile(path.join(__dirname, 'index.html'));
+ res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 app.listen(5000, () => {
    console.log('App listening on port 5000')
